d00265ea0aefd76ce541274b24ca46394db44310
Ed <scdk01@nym.hush.con>
2023-09-03 18:43:25 +0200

    new mission type

---
+++ microservices/trailer_transport_planner/src/service.py
@@ -0,0 +1,127 @@
+import math
+from flask import Flask, jsonify,  request
+import requests
+
+app = Flask(__name__)
+
+
+def call_path_planner(url, data, api_key):
+  headers = { "Authorization": f"Bearer {api_key}"}
+  response = requests.post(url, data=data, headers=headers)
+  return response
+
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status = "UP"
+    )  
+
+
+def findStep(list_of_dicts, value): 
+    for d in list_of_dicts:
+        if 'step' in d and d['step'] == value:
+            return d
+    return None
+
+
+@app.post("/plan_job/")
+def getPath():
+    """
+    Return the trajectory to helyOS which can be assigned to helyos_agent_slim_simulator.
+    """
+    # request body
+    request_body = request.get_json() 
+    request_data = request_body['request']
+    context = request_body['context']
+    service_config = request_body['_config']
+
+    step = context['orchestration']['current_step']
+    path_planner_url = service_config['path_planner_url']
+    path_planner_apikey = service_config['path_planner_apikey']
+
+    tool_id = request_data['tool_id']
+
+
+    if step == "get_trailer":
+        # Get truck data
+        helyos_tools = context['tools'] # contains all data about the agent (tool)
+        truck = next((tool for tool in helyos_tools if tool['id'] == str(tool_id)), None) # find agent in context
+        truck_position = truck['pose']    
+
+        # Get trailer
+        trailer_uuid = request_data['trailer_uuid']
+        helyos_tools = context['tools'] # contains all data about the agent (tool)
+        trailer = next((tool for tool in helyos_tools if tool['uuid'] == str(trailer_uuid)), None) # find agent in context
+        trailer_position = trailer['pose']
+
+        destination = trailer_position
+        new_request_data = {'toold_id': tool_id, 'destination': destination, 'context': context}
+        path_planner_response = call_path_planner(path_planner_url, new_request_data , path_planner_apikey)
+
+        assignment_drive_to_trailer = path_planner_response['response'][0]['assignment']
+        assignment_connect_to_trailer = f"connect_trailer {trailer_uuid}"
+        results = []
+        results.append({'tool_id': tool_id, 'assignment': assignment_drive_to_trailer})
+        results.append({'tool_id': tool_id, 'assignment': assignment_connect_to_trailer})
+
+        response =   {
+                "results":results,
+                "initial_truck_position": truck_position,
+                "initial_trailer_position": trailer_position,
+            }
+    
+
+    if step == "bring_trailer":
+        dependencies = context['dependencies']
+        get_trailer_step = findStep(dependencies, 'get_trailer')
+
+        destination = get_trailer_step['initial_truck_position']
+        new_request_data = {'toold_id': tool_id, 'destination': destination, 'context': context}
+        path_planner_response = call_path_planner(path_planner_url, new_request_data, path_planner_apikey)
+
+        assignment_drive_with_trailer = path_planner_response['response'][0]['assignment']
+        results = get_trailer_step['results']
+        results.append({'tool_id': tool_id, 'assignment': assignment_drive_with_trailer})
+
+        response =   { "results": results }
+    
+    if step == "return_trailer":
+        dependencies = context['dependencies']
+        get_trailer_step = findStep(dependencies, 'get_trailer')
+        bring_trailer_step = findStep(dependencies, 'bring_trailer')
+
+        destination = get_trailer_step['initial_trailer_position']
+        new_request_data = {'toold_id': tool_id, 'destination': destination, 'context': context}
+        path_planner_response = call_path_planner(path_planner_url, new_request_data, path_planner_apikey)
+
+        assignment_drive_back_trailer = path_planner_response['response'][0]['assignment']
+        results = bring_trailer_step['results']
+        results.append({'tool_id': tool_id, 'assignment': assignment_drive_back_trailer})
+
+        response =   { "results": results }
+
+
+    if step == "return_truck":
+        dependencies = context['dependencies']
+        get_trailer_step = findStep(dependencies, 'get_trailer')
+        bring_trailer_step = findStep(dependencies, 'bring_trailer')
+
+        destination = get_trailer_step['initial_truck_position']
+        new_request_data = {'toold_id': tool_id, 'destination': destination, 'context': context}
+        path_planner_response = call_path_planner(new_request_data, path_planner_url, path_planner_apikey)
+
+        assignment_drive_back_truck = path_planner_response['response'][0]['assignment']
+        assignment_disconnect_to_trailer = f"disconnect_trailer"
+        results = bring_trailer_step['results']
+        results.append({'tool_id': tool_id, 'assignment': assignment_drive_back_truck})
+        results.append({'tool_id': tool_id, 'assignment': assignment_disconnect_to_trailer})
+
+        response =   { "results": results, 'dispatch_order':[[0],[1],[2],[3],[4],[5]] }
+    
+    return jsonify(response)
+
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9100, debug=True)
